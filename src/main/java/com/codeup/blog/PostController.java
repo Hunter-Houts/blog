ab2f43c82deeb00e62646144e23791bb5b9d9586
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-10-29 13:50:53 -0500

    View Exercise Complete

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,22 +1,30 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "Here are all the posts";
+    public String posts(Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("First Post","This is my first post"));
+        posts.add(new Post("Second Post","This is my second post"));
+        model.addAttribute("posts",posts);
+        return "posts/index";
     }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable int id){
-        return "Here is the post with id number: " + id;
+    public String individualPost(@PathVariable int id, Model model){
+        Post post = new Post("Individual Post","this is an individual post");
+        model.addAttribute("Post",post);
+        return "posts/show";
     }
     @GetMapping("/posts/create")
     @ResponseBody
